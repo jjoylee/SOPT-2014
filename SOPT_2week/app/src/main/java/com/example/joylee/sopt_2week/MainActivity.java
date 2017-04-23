4769fb4e3e63311bf469d3ce0d0d20f65b3bb6c2
jjoy.lee <ekdnlt1995@gmail.com>
2017-04-23 20:24:40 +0900

    SOPT 2Week Seminar - Layout

---
+++ SOPT_2week/app/src/main/java/com/example/joylee/sopt_2week/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.joylee.sopt_2week;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
